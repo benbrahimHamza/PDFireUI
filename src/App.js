78f1fe7046c00914da73fe57782d754bdcb8c2c2
Hamza Benbrahim <benbrahim.hamza@gmail.com>
2017-09-24 20:01:06 +0100

    Added styles and drag drop zone with button to browse if no drag and drop is wanted :(

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import icon from './drag_drop_icon.svg';
+import Button from 'react-bootstrap/lib/Button';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>PDFire</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="file-drop container">
+          <img src={icon} alt="Drag Here" />
+          <h3> Drag your file here </h3>
+          <p> Or simply browse via this button -> <Button>Upload PDF file</Button> </p>
+        </div>
       </div>
     );
   }
