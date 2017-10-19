2a9d71d1cc899f3b5ec7b066c581d837196d05ba
Hamza Benbrahim <benbrahim.hamza@gmail.com>
2017-10-19 18:13:48 +0100

    Use component to unclutter project

--- src/App.js
+++ src/App.js
@@ -1,70 +1,16 @@
 import React, { Component } from 'react';
-import Dropzone from 'react-dropzone';
-import FileInput from 'react-file-input';
-import axios from 'axios'
-import icon from './drag_drop_icon.svg';
+import MyDropZone from './components/file_drop_zone';
+import NotificationSystem from 'react-notification-system';
 import './App.css';
-const componentClasses = ['file-drop'];
 
 class App extends Component {
-
-  // Handle file array for treatment
-  handleSelectedFiles(files){
-    for (var i = 0, len = files.length; i < len; i++) {
-        axios.post('http://localhost:8000/upload_file', {
-          pdf_file: files[i]
-        })
-        .then(function (response) {
-          alert(response);
-        })
-        .catch(function (error) {
-          alert(error);
-        });
-    }
-  }
-
-  // Trigger when file is drag in da zone
-  onDrop(acceptedFiles, rejectedFiles){
-    var files = [];
-    acceptedFiles.forEach(file => {
-        const reader = new FileReader();
-        reader.onload = () => {
-          files.push(reader.result);
-        };
-        reader.onabort = () => console.log('file reading was aborted');
-        reader.onerror = () => console.log('file reading has failed');
-
-        reader.readAsBinaryString(file);
-    });
-    this.handleSelectedFiles(files);
-  }
-
-  // Triggers when we click in the browse button
-  catchFileSelected(event){
-    var files = event.target.files;
-    this.handleSelectedFiles(files);
-  }
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1>PDFire</h1> v1.0.0
         </div>
-        <div className="main container"
-          accept='.pdf'>
-          <Dropzone onDrop={this.onDrop.bind(this)} accept='application/pdf'>
-            <img src={icon} alt="Drag Here" />
-            <h3> Drag your file here </h3>
-            <p>
-              Or simply browse by clicking anywhere in the dotted zone !
-              <p className="disclaimer">(*)Note the only PDF files are supported at the moment</p>
-                <FileInput name="myImage"
-                     accept='application/pdf'
-                     onChange={this.catchFileSelected} />
-            </p>
-          </Dropzone>
-        </div>
+        <MyDropZone/>
       </div>
     );
   }
---
+++ src/components/file_drop_zone.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import FileInput from 'react-file-input';
+import icon from '../drag_drop_icon.svg';
+import axios from 'axios';
+
+export default class MyDropZone extends React.Component {
+    // Handle file array for treatment
+    handleSelectedFiles(files){
+        var len = files.length;
+        for (var i = 0; i < len; i++) {
+            axios.post('http://pdfireapi.hamzabenbrahim.com/upload_base64', {
+                fileName: files[i]['fileName'],
+                fileSize: files[i]['fileSize'],
+                fileData: files[i]['fileData']
+            })
+            .then(function (response) {
+                console.log('Response ' + response);
+            })
+            .catch(function (error) {
+                console.log('Error ' + error);
+            });
+        }
+    }
+
+    // Trigger when file is drag in da zone
+    onDrop(acceptedFiles, rejectedFiles){
+        var files = [];
+        acceptedFiles.forEach(file => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            var fileresult = reader.result.split('base64,') ;
+            var fileData = {'fileName': file.name,'fileSize':file.size, 'fileData' : fileresult[1]};
+            files.push(fileData);
+        });
+        this.handleSelectedFiles(files);
+    }
+
+    // Triggers when we click in the browse button
+    catchFileSelected(event){
+        var files = event.target.files;
+        this.handleSelectedFiles(files);
+    }
+
+    render(){
+        return(
+            <div className="main container" accept='.pdf'>
+                <Dropzone onDrop={this.onDrop.bind(this)} accept='application/pdf'>
+                    <img src={icon} alt="Drag Here" />
+                    <h3> Drag your file here </h3>
+                    <p>
+                        Or simply browse by clicking anywhere in the dotted zone !
+                    <p className="disclaimer">(*)Note the only PDF files are supported at the moment</p>
+                    <FileInput name="myImage"
+                        accept='application/pdf'
+                        onChange={this.catchFileSelected} />
+                    </p>
+                </Dropzone>
+            </div>
+        );
+    }
+}
